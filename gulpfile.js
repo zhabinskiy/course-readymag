330e302f6582dd30c4b6bd0601f6918dd98a7a6f
Konstantin Zhabinskiy <zhabinskiy@gridly.ru>
2017-05-05 00:36:52 +0300

    First Commit

---
+++ gulpfile.js
@@ -0,0 +1,102 @@
+'use strict';
+
+const gulp = require('gulp'),
+      sass = require('gulp-sass'),
+      postcss = require('gulp-postcss'),
+      autoprefixer = require('autoprefixer'),
+      babel = require('gulp-babel'),
+      concat = require('gulp-concat'),
+      uglify = require('gulp-uglify'),
+      plumber = require('gulp-plumber'),
+      broweserSync = require('broweser-sync'),
+      reload = browserSync.reload;
+
+const paths = {
+  styles: 'source/styles/',
+  css: 'dist/css/',
+  scripts: 'source/scripts/',
+  js: 'dist/js',
+  templates: 'source/templates/',
+  html: 'dist/',
+};
+
+// Одноразовая сборка проекта
+gulp.task('build', function() {
+  gulp.start('templates', 'styles', 'scripts', 'cache', 'img', 'fonts');
+});
+
+// Запуск живой сборки
+gulp.task('live', function() {
+  gulp.start('templates', 'styles', 'scripts', 'img', 'cache', 'watch', 'server');
+});
+
+// Следим за изменениями файлов
+gulp.task('watch', function() {
+  gulp.watch(paths.templates + '**/*.html', ['templates']);
+  gulp.watch(paths.styles + '**/*.scss', ['styles']);
+  gulp.watch(paths.scripts + '*.js', ['scripts']);
+});
+
+// Собираем HTML
+gulp.task('templates', function() {
+  gulp.src(paths.templates + '*.html')
+    .pipe(plumber({errorHandler: onError}))
+    .pipe(gulp.dest(paths.html))
+    .pipe(reload({stream: true}));
+});
+
+// Собираем SASS
+gulp.task('styles', function() {
+  return gulp.src(paths.styles + 'main.scss')
+    .pipe(sass({
+      outputStyle: 'compressed',
+      errLogToConsole: true
+    }).on('error', onError))
+    .pipe(postcss(autoprefixer))
+    .pipe(gulp.dest(paths.css));
+});
+
+// Собираем JS
+gulp.task('scripts', function() {
+  gulp.src(paths.scripts + '*.js')
+    .pipe(plumber({errorHandler: onError}))
+    .pipe(babel())
+    .pipe(concat('scripts.js'))
+    .pipe(uglify())
+    .pipe(gulp.dest(paths.js));
+});
+
+// Локальный сервер
+gulp.task('server', function() {
+  portfinder.getPort(function(err, port) {
+    browserSync({
+      server: {
+        baseDir: ".",
+        serveStaticOptions: {
+          extensions: ['html']
+        }
+      },
+      host: 'localhost',
+      notify: false,
+      port: port
+    });
+  });
+});
+
+// Обновление страниц
+gulp.task('html', function() {
+  gulp.src(paths.html + '*.html')
+    .pipe(reload({stream: true}));
+});
+
+// Ошибки
+const onError = function(error) {
+  gutil.log([
+    (error.name + ' in ' + error.plugin).bold.red,
+    '',
+    error.message,
+    ''
+  ].join('\n'));
+  gutil.beep();
+  this.emit('end');
+};
